496e559a009422154bb1fea343aac9532ef0d0fe
Sean Logsdon <seanlogsdon14@gmail.com>
2020-11-09 23:25:02 -0600

    Initial Commit

---
+++ Landmarks_SAL/Landmarks_SALApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_SALApp.swift
+//  Landmarks_SAL
+//
+//  Created by Sean Logsdon on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_SALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
